fe90fc7d18cc0e7b7b78d92c53c2a9205c7a28bb
mlafon <m.lafon@quinten-france.com>
2020-01-09 10:53:57 +0100

    fix folds

--- load.py
+++ load.py
@@ -3,7 +3,9 @@ import os
 from torch.utils.data import Dataset
 from PIL import Image
 from imgaug.augmentables.bbs import BoundingBox
-import utils as u
+import pandas as pd
+from utils import parse_annotation
+
 
 class Data(object):
     def __init__(self, img, img_id, fold, label="TemoinNone", bboxe=[0, 0, 0, 0]):
@@ -16,57 +18,39 @@ class Data(object):
 
 
 class MichelinDataset(Dataset):
-    def __init__(self, root, transforms=None):
+    def __init__(self, root, folds, transforms=None):
+        """
+
+        :param root: path to folder containing input images and targets
+        :param folds: path to csv file containing folds table
+        :param transforms:
+        """
         self.root = root
+        self.folds = folds
         self.transforms = transforms
         # load all image files, sorting them to
         # ensure that they are aligned
-
         # Get names of all image and annotation files
         self.files_imgs = list(sorted([img_file for img_file in os.listdir(root) if img_file.endswith(".jpg")]))
         self.files_annotation = list(sorted([img_file for img_file in os.listdir(root) if img_file.endswith(".txt")]))
 
         # Load cross val folds table
-        self.file_folds = os.path.join(self.root, "cross_val_folds.csv")
-        self.table_folds = u.load_folds_table(path_folds)
+        self.table_folds = pd.read_csv(self.folds, sep=",")
 
     def __getitem__(self, idx):
         # load images ad masks
         path_img = os.path.join(self.root, self.files_imgs[idx])
         path_annotation = os.path.join(self.root, self.files_annotation[idx])
-        path_folds = os.path.join(self.root, "cross_val_folds.csv")
         img = Image.open(path_img)  # .convert("RGB")
-        label, bboxe = self.parse_annotation(path_annotation=path_annotation)
-        fold = self.add_folds(table_folds=self.table_folds, img_id=self.files_imgs[idx][:-4])
+        img_id = self.files_imgs[idx][:-4]
+        raw_img_id = "_".join(img_id.split("_")[:2])
+        label, bboxe = parse_annotation(path_annotation=path_annotation)
+        fold = self.table_folds[self.table_folds["img_id"] == raw_img_id]["fold"].values[0]
 
         if self.transforms:
             img, bboxe = self.transforms(img, bboxe)
 
-        return Data(img,
-                    self.files_imgs[idx][:-4],
-                    fold,
-                    label,
-                    bboxe)
+        return Data(img, img_id, fold, label, bboxe)
 
     def __len__(self):
         return len(self.files_imgs)
-
-    def add_folds(self, table_folds, img_id):
-        fold_num = table_folds[table_folds["img_id"]==img_id]["fold"].values[0]
-        return fold_num
-
-    def parse_annotation(self, path_annotation):
-        f = open(path_annotation, 'r')
-        content = f.read()
-        infos = content.split(',')
-
-        label = infos[0]
-        label = label.replace('"', "").replace('[', "")
-
-        xmin = int(infos[1].replace("[", "").lstrip())
-        ymin = int(infos[2].lstrip())
-        xmax = int(infos[3].lstrip())
-        ymax = int(infos[4].replace("]", "").lstrip())
-
-        f.close()
-        return label, [xmin, ymin, xmax, ymax]
--- utils.py
+++ utils.py
@@ -14,12 +14,24 @@ def save_targets(bboxe: BoundingBox, filepath: str):
     f.close()
 
 
+def parse_annotation(path_annotation):
+    f = open(path_annotation, 'r')
+    content = f.read()
+    infos = content.split(',')
+
+    label = infos[0]
+    label = label.replace('"', "").replace('[', "")
+
+    xmin = int(infos[1].replace("[", "").lstrip())
+    ymin = int(infos[2].lstrip())
+    xmax = int(infos[3].lstrip())
+    ymax = int(infos[4].replace("]", "").lstrip())
+
+    f.close()
+    return label, [xmin, ymin, xmax, ymax]
+
+
 def extract_bboxe_img(img, bboxe: BoundingBox):
     raw_img, bboxe = np.array(img), bboxe
     x1, y1, w, h = bboxe.x1, bboxe.y1, bboxe.width, bboxe.height
     return raw_img[y1:y1 + h, x1:x1 + w, :]
-
-
-def load_folds_table(path_folds):
-    df = pd.read_csv(path_folds, sep=",")
-    return df
